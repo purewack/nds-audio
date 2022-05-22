3f1e5ff79721c9c1e4ee40dd3326df59b9a9108c
purewack <bobimaster15@gmail.com>
2022-05-22 19:38:48 +0100

    recreated felix-ii graphic :)

--- source/gfx.c
+++ source/gfx.c
@@ -2,151 +2,88 @@
 #include <nds.h>
 #include <gl2d.h>
 #include "gfx.h"
-#include "tt.h"
+#include "set.h"
 
-// struct Anim {
-//     Texture2D tex;
-//     Rectangle src;
-//     Rectangle dest;
-//     Vector2 orig;
-//     Color tint;
-//     int32 rot;
-//     int32 scale;
 
-//     int animating;
-//     int frames;
-//     int tilesx;
-//     int tilesy;
-//     int frameCtr;
-//     int animSpeed;
-//     int timeCtr;
+glImage tex[64];
 
-//     int32 velx;
-//     int32 vely;
-//     int32 velrot;
-//     int tp;
-//     int dead;
-// } ;
+#define STAR_INDEX 57
+#define STAR_FRAME_COUNT 5
 
-// struct Anim sprites[256];
-// const unsigned int screenWidth = 240;
-// const unsigned int screenHeight = 160;
+struct Anim {
+    int32 dest_x;
+	int32 dest_y;
+    int32 rot;
+    int32 scale;
 
-// void initAnim(struct Anim *a,char *texture, int tilesx, int tilesy){
+    u8 animating;
+    u8 frames;
+    u8 sprite;
+    u8 frameCtr;
+    int animSpeed;
+    int timeCtr;
 
-//     if(texture != NULL) a->tex = LoadTexture(texture);
+    int32 velx;
+    int32 vely;
+    int32 velrot;
+    int tp;
+    int dead;
+} ;
 
-//     a->frames = tilesx*tilesy;
-//     a->tilesx = tilesx;
-//     a->tilesy = tilesy;
+struct Anim sprites[64];
+int spritesCount = 0;
+const int screenBounds = 332;
+const int velx = 1;
 
-//     a->src = (Rectangle){0.0f, 0.0f, a->tex.width / a->tilesx, a->tex.height / a->tilesy};
-//     a->dest = (Rectangle){0.0f, 0.0f, a->src.width, a->src.height};
-//     a->orig = (Vector2){(float) a->src.width/2.0f ,(float)a->src.height / 2.0f};
+void initAnim(struct Anim *a, int sprite, int frame_count){
+	a->sprite = sprite;
+    a->frames = frame_count;
+    a->frameCtr = 0;
+    a->animating = 1;
+    a->animSpeed = 15;
+	a->dest_x = 0;
+	a->dest_y = 0;
+    a->velx = 0;
+    a->vely = 0;
+    a->velrot  = 0;
+    a->scale = 1<<12;
+    a->tp = 1;
+    spritesCount++;
+}
+
+void nextFrame(struct Anim* a){
+	
+    a->timeCtr++;
+    if(a->timeCtr > a->animSpeed && a->animating) {
+        a->timeCtr = 0;
+        a->frameCtr = ((a->frameCtr+1) % a->frames);
+    }
     
-//     a->frameCtr = 0;
-//     a->animating = 1;
-//     a->tint = WHITE;
-//     a->animSpeed = 15;
-//     a->velx = 0.0f;
-//     a->vely = 0.0f;
-//     a->velrot  = 0.0f;
-//     a->scale = 1.0f;
-//     a->tp = 1;
+    a->rot    += a->velrot;
+    a->dest_x += (a->velx*a->scale);
+    a->dest_y += (a->vely*a->scale);
+
+    if((a->dest_x>>12) > screenBounds) { 
+		a->dest_x = (-50)<<12;  
+		a->frameCtr = rand()%a->frames;
+	}
     
-// }
+}
 
-// void nextFrame(struct Anim* a){
-//     a->timeCtr++;;
-//     if(a->timeCtr > a->animSpeed && a->animating) {
-//         a->timeCtr = 0;
-//         a->frameCtr++;
-//         if(a->frameCtr >= a->frames){
-//             a->frameCtr = 0;
-//         }
-//     }
-    
-//     int f = a->frameCtr;
-//     int yoff = (f/a->tilesx);
-// 	int xoff = (f-(yoff*a->tilesx));
-
-//     a->src.x = a->src.width * xoff;
-//     a->src.y = a->src.height * yoff;
-//     a->rot    += a->velrot;
-//     a->dest.width = a->scale * a->src.width;
-//     a->dest.height = a->scale * a->src.height;
-//     a->orig.x = a->dest.width/2;
-//     a->orig.y = a->dest.height/2;
-//     a->dest.x += (a->velx*a->scale);
-//     a->dest.y += (a->vely*a->scale);
-    
-
-//     if(a->tp == 1){
-//         if(a->dest.x < 0 - screenBounds/2)      { a->dest.x = (screenWidth + screenBounds/2) - 1;}
-//         else if(a->dest.x > screenWidth + screenBounds/2) { a->dest.x = (1 - screenBounds/2);  a->frameCtr = (float)(rand()%a->frames);}
-//         //if(a->dest.y < -50.0f)                          { a->dest.y = (float)screenHeight + 50.0f;}
-//         //if(a->dest.y > (float)screenHeight)     { a->dest.y = -50.0f;}
-        
-//     } 
-    
-// }
-
-// void drawAnim(struct Anim *a){
-//     if(!a->dead)
-//     {
-//         DrawTexturePro(a->tex, a->src, a->dest, a->orig, a->rot, a->tint);
-//     }
-// }
-
-// void stars(){ 
-//     for(int i=0; i<32; i++){
-//         //sprites[j].tex = starstex;
-//         //initAnim(&sprites[j],NULL,5,1);
-//         //spritesCount++;
-//         sprites[i].dest.x = (float)(rand()%screenWidth);
-//         sprites[i].dest.y = (float)(rand()%screenHeight);
-//         //sprites[j].frameCtr = (float)(rand()%sprites[j].frames);
-//         //sprites[j].velrot  = VEL_SPEED_ROT;
-//         //j += 1;
-//     }
-//     transition();
-//     #define FOV 4.0f
-//     screenBounds = screenWidth*1.3;
-    
-
-//         BeginTextureMode(outputCanvas);
-//         //DrawRectangle(0, 0, screenWidth, screenHeight, (Color) {0,0,0,32} );          
-//         ClearBackground(BLACK);
-//         for(int i=0; i<spritesCount; i++){
-            
-            
-//             if(i >= 32){
-//                 if(sprites[i].dest.x > -50 && sprites[i].dest.x < screenWidth + 250){
-//                     sprites[i].scale =  FOV + (  ((float)(screenWidth) - sprites[i].dest.x) / (float)(screenWidth)*(1.0f - FOV));
-//                 }
-//                 else sprites[i].scale = 1.0f;
-//                 //int tint = 127 + (int) ( 127.0f * (sprites[i].dest.x+50.0f / (float)(screenWidth+100.f)));
-//                 //sprites[i].tint = (Color){tint,tint,tint,255};
-//             }
-//             else{
-//                 sprites[i].scale = 0.5f;
-//             }
-
-
-//             drawAnim(&sprites[i]);
-//             nextFrame(&sprites[i]);
-//         }
-        
-//         EndTextureMode();
-//         DrawScreen();
-    
-
-// }
-glImage* tex;
-s16 xx;
+void drawAnim(struct Anim *a){
+    if(!a->dead)
+    {
+        glSpriteRotateScale(
+			a->dest_x>>12,
+			a->dest_y>>12,
+			a->rot,
+			a->scale<<1,
+			GL_FLIP_NONE,
+			&tex[a->sprite + a->frameCtr]);
+	}
+}
 
 void initFrame(){
-    xx = 0;
     //set mode 5, enable BG0 and set it to 3D
 	videoSetMode( MODE_5_3D );
 	videoSetModeSub( MODE_0_2D  );
@@ -184,36 +121,60 @@ void initFrame(){
 	
 
 	//important, tex must be pointer or prealocated space w*h * frames count !!!!
-	 int tid = 
-		glLoadTileSet( tex,			// pointer to glImage array
+	glLoadTileSet( tex,			// pointer to glImage array
 					   16,				// sprite width
 					   16,				// sprite height
-					   32,				// bitmap image width
-					   32,				// bitmap image height
+					   128,				// bitmap image width
+					   128,				// bitmap image height
 					   GL_RGB256,		// texture type for glTexImage2D() in videoGL.h
-					   TEXTURE_SIZE_32,	// sizeX for glTexImage2D() in videoGL.h
-					   TEXTURE_SIZE_32,	// sizeY for glTexImage2D() in videoGL.h
+					   TEXTURE_SIZE_128,	// sizeX for glTexImage2D() in videoGL.h
+					   TEXTURE_SIZE_128,	// sizeY for glTexImage2D() in videoGL.h
 					   GL_TEXTURE_WRAP_S|GL_TEXTURE_WRAP_T|TEXGEN_OFF|GL_TEXTURE_COLOR0_TRANSPARENT,
-					   16,					// Length of the palette to use (16 colors)
-					   (u16*)ttPal,		// Load our 256 color tiles palette
-					   (u8*)ttBitmap // image data generated by GRIT
+					   256,					// Length of the palette to use (16 colors)
+					   (u16*)setPal,		// Load our 256 color tiles palette
+					   (u8*)setBitmap // image data generated by GRIT
 					 );
+ 
+	for(int j=0; j<32; j++){
+        initAnim(&sprites[j],STAR_INDEX,STAR_FRAME_COUNT);
+        sprites[j].dest_x = (rand()%SCREEN_WIDTH)<<12;
+        sprites[j].dest_y = (rand()%SCREEN_HEIGHT)<<12;
+        sprites[j].frameCtr = rand()%sprites[j].frames;
+        sprites[j].rot  = rand()%2000;
+        sprites[j].velrot  = -100 + (rand()%150);
+    }
+	for(int j=32; j<32+6; j++){
+        initAnim(&sprites[j],0, STAR_INDEX-2);
+		sprites[j].animating = 0;
+		sprites[j].velx = velx;
+		sprites[j].frameCtr = rand()%(STAR_INDEX-1);
+        sprites[j].dest_x = ((j-32)*64)<<12;
+        sprites[j].dest_y = (SCREEN_HEIGHT>>1)<<12;
+        sprites[j].rot  = rand()%2000;
+        sprites[j].velrot  = -100 + (rand()%10);
+    }
 }
+
 
 void onFrame(){
     glBegin2D();
 
-    const int v = (1<<12);
-    int c = 16 + (sinLerp(xx+=256)>>8);
+    glBoxFilled(0,0,SCREEN_WIDTH,SCREEN_HEIGHT,RGB15(0,0,2));
 
-    glBoxFilledGradient( 0, 0, 255, 191,
-								 RGB15(c,0,0),
-								 0,
-								 RGB15(c,0,0),
-                                 0
-                               );
+	for(int i=0; i<spritesCount; i++){
+		
+		
+		if(i >= 32){
+			int xx = sprites[i].dest_x>>12;
+			sprites[i].scale =  (1<<12) + (xx*20);
+		}
+		else{
+			sprites[i].scale = (1<<11) - 300;
+		}
 
-	glSpriteScale(30,30,1<<14,GL_FLIP_NONE,&tex[3]);
+		drawAnim(&sprites[i]);
+		nextFrame(&sprites[i]);
+	}
 
     glEnd2D();
     glFlush(0);
